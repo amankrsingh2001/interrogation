fcfc635af3bdb283d8e38f364b0d20aca98ad2f3
Amansingh <singhamankr17@gmail.com>
2025-01-14 01:59:05 +0530

    deployment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Inputbox from "@/components/Inputbox";
-import ResumeDrop from "@/components/Inputbox";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-        <Inputbox/>
+        This is the home page
     </div>
   );
 }
